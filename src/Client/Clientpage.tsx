e1779fb5649c921783ac0bc60dbf94ce410c9579
Manifestus <link_figueroa20@hotmail.com>
2022-03-04 18:35:49 -0600

    Button Fix

--- src/Client/Clientpage.tsx
+++ src/Client/Clientpage.tsx
@@ -178,7 +178,7 @@ export const Clientpage: FC<IProps> = (props) => {
         flexDirection="column"
       >
         <Flex h="25rem" w="80vw" bgColor="#F2E0DF " flexDirection="column">
-          <Button alignSelf="flex-end">{AddClient()}</Button>
+          <Flex w='10vw'>{AddClient()}</Flex>
           <Table variant="simple">
             <TableCaption>Client Creation Table</TableCaption>
             <Thead>
